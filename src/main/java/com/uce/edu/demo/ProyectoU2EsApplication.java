22a19733a2b4301d1d94b49b285d949c892ab3c7
Erick Solano <solano_erick@hotmail.com>
2022-06-21 19:51:16 -0500

    Creacion Proyecto U2

---
+++ src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU2EsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU2EsApplication.class, args);
+	}
+
+}
