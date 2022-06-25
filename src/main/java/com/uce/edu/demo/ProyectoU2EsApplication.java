def8c418be8c893216e568dd123df78f97016d14
Erick Solano <solano_erick@hotmail.com>
2022-06-24 20:02:26 -0500

    Taller14 configuraciones BD

--- src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
@@ -1,13 +1,20 @@
 package com.uce.edu.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoU2EsApplication {
+public class ProyectoU2EsApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
 	}
 
 }
