426f45f3a01d8ac4cc3c490a1a449d94511539c7
Erick Solano <solano_erick@hotmail.com>
2022-06-26 17:57:45 -0500

    tarea12 config log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
@@ -1,12 +1,15 @@
 package com.uce.edu.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoU2EsApplication implements CommandLineRunner{
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProyectoU2EsApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EsApplication.class, args);
 	}
@@ -14,7 +17,7 @@ public class ProyectoU2EsApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		LOGGER.info("HOLA MUNDO");
 	}
 
 }
