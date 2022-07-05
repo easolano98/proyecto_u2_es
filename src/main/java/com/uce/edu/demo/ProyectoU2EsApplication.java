968dc3e005fb37d94e8b3396137c346ef52ce6a5
Erick Solano <solano_erick@hotmail.com>
2022-07-04 20:17:04 -0500

    Taller 17 Jpay Mapeo

--- src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2EsApplication.java
@@ -1,24 +1,21 @@
 package com.uce.edu.demo;
 
-
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.estudiante.repository.modelo.Estudiante;
-import com.uce.edu.demo.estudiante.service.IEstudianteJpaService;
+import com.uce.edu.demo.repository.modelo.Persona;
+import com.uce.edu.demo.service.IPersonaJpaService;
 
 @SpringBootApplication
-public class ProyectoU2EsApplication implements CommandLineRunner{
+public class ProyectoU2EsApplication implements CommandLineRunner {
 	private static final Logger LOGGER = Logger.getLogger(ProyectoU2EsApplication.class);
-	
-	
-	@Autowired 
-	private IEstudianteJpaService estudianteJpaService;
-	
+
+	@Autowired
+	private IPersonaJpaService jpaService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2EsApplication.class, args);
 	}
@@ -26,32 +23,29 @@ public class ProyectoU2EsApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		//Buscar
-		LOGGER.info(this.estudianteJpaService.buscarCarnet(1));
-	
-		Estudiante estudiante=new Estudiante();
-		estudiante.setNumCarnet(5);
-		estudiante.setCedula("0720131021");
-		estudiante.setNombre("Janineth");
-		estudiante.setApellido("Iza");
-		estudiante.setCarrera("Medicina");
-		
-		//Guardar
-		this.estudianteJpaService.guardar(estudiante);
-		
-		Estudiante estudiante1=new Estudiante();
-		estudiante1.setNumCarnet(3);
-		estudiante1.setCedula("0506152022");
-		estudiante1.setNombre("Leonardo");
-		estudiante1.setApellido("Simbaña");
-		estudiante1.setCarrera("Ingenieria Civil");
-		//Actualizar
-		this.estudianteJpaService.actualizar(estudiante1);
-		//Eliminar
-		this.estudianteJpaService.eliminar(2);
+
+		//LOGGER.info(this.jpaService.buscarPorId(3));
+
+		Persona per = new Persona();
+		//per.setId(3);
+		per.setNombre("Pamela1");
+		per.setApellido("Nogales");
+
+		// GUARDAR
+		 this.jpaService.guardar(per);
+
+		Persona per1=new Persona();
+		per1.setId(8);
+		per1.setNombre("Wilsona");
+		per1.setApellido("Tipantaxi");
 		
 		
-		
+		// ACTUALIZAR
+		//this.jpaService.actualizar(per1);
+
+		// ELIMINAR
+		//this.jpaService.eliminar(3);
+
 	}
 
 }
